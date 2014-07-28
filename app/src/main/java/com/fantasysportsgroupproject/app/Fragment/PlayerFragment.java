e153b9f67861779f8a6b9c22837592f737ee8be0
Anu <anusubra@yahoo.com>
2014-07-28 16:24:34 -0700

    fixed list view issue

--- app/src/main/java/com/fantasysportsgroupproject/app/Fragment/PlayerFragment.java
+++ app/src/main/java/com/fantasysportsgroupproject/app/Fragment/PlayerFragment.java
@@ -3,7 +3,6 @@ package com.fantasysportsgroupproject.app.Fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +10,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.fantasysportsgroupproject.app.Helpers.MySingleton;
 import com.fantasysportsgroupproject.app.Helpers.PlayerSearchListArrayAdapter;
 import com.fantasysportsgroupproject.app.R;
 import com.parse.FindCallback;
@@ -51,7 +48,6 @@ public class PlayerFragment extends Fragment{
     private ArrayList<Player> arrayOfPlayers;
     private PlayerSearchListArrayAdapter aPlayerListAdapter; // to show the list
 
-
     // This event fires 1st, before creation of fragment or any views
     // The onAttach method is called when the Fragment instance is associated with an Activity instance.
     // This does not mean the Activity is fully initialized.
@@ -72,45 +68,19 @@ public class PlayerFragment extends Fragment{
         setup();
         setupListener();
 
-        /* since we are creating the array within the inner class, this gives null pointer
-        exception. I'm going to see creating the adapter within the inner class helps?
-         */
-      /*  aPlayerListAdapter= new PlayerSearchListArrayAdapter(getActivity(),arrayOfPlayers);
-        lvPlayerList = (ListView) view.findViewById(R.id.lvPlayerList);
-        lvPlayerList.setAdapter(aPlayerListAdapter);*/
-        //aPlayerListAdapter.clear();
-
-//        aPlayerListAdapter.notifyDataSetChanged();
         return view;
     }
-
-
 
     private void setup() {
         etPlayerNameSearch = (EditText) view.findViewById(R.id.etPlayerNameSearch);
         givenFirstName = etPlayerNameSearch.getText().toString();
         btnSearch = (Button) view.findViewById(R.id.btnSearch);
-
-        /* TESTED UPDATES array of players within the done class, but not able to see
-        outside of done method for adapter
-
-        newPlayer = MySingleton.getInstance().newPlayer;
-        arrayOfPlayers = MySingleton.getInstance().arrayOfPlayers; */
-
-
-
-       // lvPlayerList = (ListView) view.findViewById(R.id.lvPlayerList);
-       // arrayOfPlayers = MySingleton.getInstance().arrayOfPlayers;
-      // aPlayerListAdapter= new PlayerSearchListArrayAdapter(getActivity(),arrayOfPlayers);
-       // lvPlayerList.setAdapter(aPlayerListAdapter);
-
     }
 
     private void setupListener()
     {
         btnSearch.setOnClickListener(btnSearchListener);
 
-        //lvPlayerList.setOnItemClickListener(playerListListener);
     }
 
     // on click of the list, show the detailed page with stats for the player
@@ -121,19 +91,16 @@ public class PlayerFragment extends Fragment{
        public void onItemClick(AdapterView<?> adapterView,View view, int position,long id) {
             // create a bundle for args
             // pass player id to Anna
-
-
-
             // How to get the item at that position
-            //getItem(position);
-            Toast.makeText(getActivity(),"On Click list - activate detailed player activity", Toast.LENGTH_SHORT).show();
+           // view.getId();
+          //  Toast.makeText(getActivity(),"On Click list - activate detailed player activity", Toast.LENGTH_SHORT).show();
             // uncomment later =========   todo????
-           /* Intent i = new Intent(DetailedPlayerListActivity.class,"Showing Stats");
+         /*   Intent i = new Intent(DetailedPlayerListActivity.class,"Showing Stats");
             i.putInt(newPlayer.getPlayerID());
-            i.putString(newPlayer.getFantasyPosition()); */
+            i.putString(newPlayer.getFantasyPosition());
 
             //startActivityForResult(intent, DISPLAY_STATS_REQUEST_CODE);
-            // After passing the player id to Activity, DO I need result from Anna
+            // After passing the player id to Activity, DO I need result from Anna */
 
 
         }
@@ -144,138 +111,68 @@ public class PlayerFragment extends Fragment{
     private View.OnClickListener btnSearchListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
-            //TRYINg to see if this helps to see the array of players available outside of done?
-            // THis works tested.
-
-           // newPlayer = MySingleton.getInstance().newPlayer;
-           // arrayOfPlayers = MySingleton.getInstance().arrayOfPlayers;
-           // aPlayerListAdapter = MySingleton.getInstance().aPlayerListAdapter;
-
             searchPlayer(givenFirstName);
-
-            /* FIND OUT WHY? the array of players though it is declared as a global instance,
-            it still doesn't show outside the anonymous class-The following code does not work ----
-            TODO
-            searchPlayer(givenFirstName);
-            Log.d(TAG,"Printing the array list from search button");
-            Log.d(TAG,"print the array list size  : " + arrayOfPlayers.size());
-            Log.d(TAG,"PRINT THE FINAL LIST OF PLAYERS ");
-            for (Player p:arrayOfPlayers) {
-                Log.d(TAG,"print the array list of players from search button: " + p.getPlayerID());
-            }*/
-
-
         }
     };
 
     public void searchPlayer(String playerFirstName)
     {
-
         ParseQuery<ParseObject> query_players = ParseQuery.getQuery("NFLPlayers");
 
-       // newPlayer = MySingleton.getInstance().newPlayer;
-       // arrayOfPlayers = MySingleton.getInstance().arrayOfPlayers;
-        // Making the adapter FINAL DID NOT WORK
-       // final PlayerSearchListArrayAdapter aPlayerListAdapter =
-       //         new PlayerSearchListArrayAdapter(getActivity(),arrayOfPlayers);
-
         try {
-            // query_players.whereEqualTo("FirstName", playerFirstName); // change to where matches
             query_players.whereMatches("FirstName","Patrick");
         } catch (NullPointerException e) {
             e.printStackTrace(); // change this to meaning ful message
 
         }
 
-        //playerArrayList.add(null);
         query_players.findInBackground(new FindCallback<ParseObject>()
         {
-            public String shortName;
+            public String shortName,firstName,lastName;
             public int playerID;
 
             @Override
             public void done(List<ParseObject> playerList, com.parse.ParseException e) {
-               // aPlayerListAdapter= new PlayerSearchListArrayAdapter(getActivity(),arrayOfPlayers);
+
+                arrayOfPlayers = new ArrayList<Player>();
+                lvPlayerList = (ListView) view.findViewById(R.id.lvPlayerList);
+                aPlayerListAdapter= new PlayerSearchListArrayAdapter(getActivity(),arrayOfPlayers);
+                lvPlayerList.setAdapter(aPlayerListAdapter);
+
                 if (e == null)
                 {
                     d("TAG", "Retrieved: size =  " + playerList.size());
 
                     for(int i=0;i<playerList.size();i++)
                     {
-                        newPlayer = MySingleton.getInstance().newPlayer;
-                        arrayOfPlayers = MySingleton.getInstance().arrayOfPlayers;
-
-                        // short name is abbreviated name; got it from Parse
-                        // create a single player
-                        d("TAG", "FirstName =  " + playerList.get(i).getString("FirstName"));
-                        String shortName = playerList.get(i).getString("ShortName");
-                        String firstName = playerList.get(i).getString("FirstName");
-                        String lastName = playerList.get(i).getString("LastName");
+                        shortName = playerList.get(i).getString("ShortName");
+                        firstName = playerList.get(i).getString("FirstName");
+                        lastName = playerList.get(i).getString("LastName");
                         playerID = playerList.get(i).getInt("PlayerID");
 
-                        Log.d("Player", "short name: " + shortName);
-
+                        // create a single player
+                        newPlayer = new Player();
                         newPlayer.setPlayerShortName(shortName);
                         newPlayer.setPlayerFirstName(firstName);
                         newPlayer.setPlayerLastName(lastName);
                         newPlayer.setPlayerID(playerID);
                         newPlayer.setPhotoUrl(playerList.get(i).getString("PhotoUrl"));
-
-                        Log.d("Player", "the photo url = : " + newPlayer.getPhotoUrl());
                         newPlayer.setFantasyPosition(playerList.get(i).getString("FantasyPosition"));
 
                         if (newPlayer != null) {
-                            d("Player", "adding the new player to arraylist ");
                             arrayOfPlayers.add(newPlayer);
-
-                           Log.d("Player", "the new player's last name  in the arrayList " + arrayOfPlayers.get(i).getPlayerLastName());
-                           Log.d("Player", "the new player's photo url  in the arrayList " + arrayOfPlayers.get(i).getPhotoUrl());
-
                         }
-                        //printing the playerlist:
-                        for(Player x:arrayOfPlayers) {
-                            Log.d(TAG,"printing the list of player's last name within the for: " + x.getPlayerLastName());
-                        }
-
-
-                        lvPlayerList = (ListView) view.findViewById(R.id.lvPlayerList);
-                        aPlayerListAdapter= new PlayerSearchListArrayAdapter(getActivity(),arrayOfPlayers);
-                        lvPlayerList.setAdapter(aPlayerListAdapter);
-                        aPlayerListAdapter.notifyDataSetChanged();
-
                     }// end of for
-
-                    /* THIS WORKS WITHIN THE INNER CLASS --- TESTED)
-                    Log.d("Player", "the array size " + arrayOfPlayers.size());
-                    Log.d("Player", "the player id from the arrayList " + arrayOfPlayers.get(8).getPlayerID());
-                    Log.d("Player"," the activity in the player fragment: " + getActivity());
-
-                    */
-
-                    // UNCOMMENT THIS LATER IF NECESSARY-----
-
-                    // Adapter has to be within the inner class done, otherwise it does not
-                    // get updated.
-
-                   /*lvPlayerList = (ListView) view.findViewById(R.id.lvPlayerList);
-                   lvPlayerList.setAdapter(aPlayerListAdapter);
-                   aPlayerListAdapter.notifyDataSetChanged(); */
-
-                }
-
+                    aPlayerListAdapter.notifyDataSetChanged();
+                } //end of (e == null)
                 else
                 {
                     d("score", "Error: " + e.getMessage());
-                    //playerArrayList = null;
-
                 }
             }// end of done
 
         }); // end of inner class
 
-      // Log.d(TAG, "Printing playerArrayList after coming out of done : " + arrayOfPlayers.size());
-       Log.d("Player", " the activity in the player fragment: " + getActivity());
-    }
+    }//end of searchPlayer()
 
 }
--- app/src/main/java/com/fantasysportsgroupproject/app/Helpers/PlayerSearchListArrayAdapter.java
+++ app/src/main/java/com/fantasysportsgroupproject/app/Helpers/PlayerSearchListArrayAdapter.java
@@ -1,7 +1,6 @@
 package com.fantasysportsgroupproject.app.Helpers;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,31 +28,16 @@ public class PlayerSearchListArrayAdapter extends ArrayAdapter<Player>{
     private TextView tvPlayerFullName;
     private TextView tvPlayerPosition;
 
-    List<Player> playerList;
-    LayoutInflater mInflater;
+    public PlayerSearchListArrayAdapter(Context context,List<Player> playerList) {
+         super(context,R.layout.player_list_item,playerList);
 
-     public PlayerSearchListArrayAdapter(Context context,List<Player> playerList) {
-         //super(context, 0 , playerList);
-         super(context,0,playerList);
-         mInflater = LayoutInflater.from(context);
-         this.playerList = playerList;
      }
 
 
     public View getView(int position, View convertView, ViewGroup parent) {
-        //return super.getView(position, convertView, parent);
-
-        Log.d(TAG,"inside adapter");
-
-        Log.d(TAG,"position = " + position);
-        Log.d(TAG,"activity = " + getContext());
-
 
         // get the data item for position
         Player player = getItem(position);
-        Log.d(TAG,"player list size from within the adapter: = " + playerList.size());
-        Log.d(TAG,"player's last name = " + player.getPlayerLastName());
-        Log.d(TAG,"player's id: 8th in  the list " + playerList.get(8).getPlayerID());
 
         // find if the view exists. what that means if we reached the max limit
         // of items to view on the screen, then Android starts to recycle the
@@ -64,33 +48,20 @@ public class PlayerSearchListArrayAdapter extends ArrayAdapter<Player>{
         if (v == null)
         {
             LayoutInflater inflater = LayoutInflater.from(getContext());
-           // v = inflater.inflate(R.layout.player_list_item,parent,false);
-           // check if to attach to root
             v = inflater.inflate(R.layout.player_list_item,parent,false);
 
         }
-
-
-        // find the ids in the tweet_item.xml
+        // find the ids
         ImageView imvPlayer = (ImageView)v.findViewById(R.id.imvPlayer);
         TextView tvPlayerFullName = (TextView)v.findViewById(R.id.tvPlayerFullName);
         TextView tvPlayerPosition = (TextView)v.findViewById(R.id.tvPlayerPosition);
 
-        // using the imageLoader to load the image
-        //ImageLoader imgLoader = ImageLoader.getInstance();
-        //imgLoader.displayImage(tweet.getTweetUser().getProfileImageUrl(),imvProfileImage);
-
         // populate the items
         // ImageUri is the url of the image
         String imageUri = player.getPhotoUrl();
-        Log.d(TAG,imageUri);
-     //   String imageUri = "http://static.nfldata.com/images/player/1944.jpg";
-        //getTweetUser().getProfileImageUrl();
         Picasso.with(getContext()).load(imageUri).into(imvPlayer);
-       // tvPlayerFullName.setText(player.getPlayerFirstName() + " " + player.getPlayerLastName());
         tvPlayerFullName.setText(player.getPlayerShortName());
         tvPlayerPosition.setText(player.getFantasyPosition());
-
 
         return v;
 
